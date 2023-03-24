dc312f2bb65ed6ceb70583a52944bcab3677dc9c
gabriel-vitebo <vitebo@outlook.com>
2023-03-24 14:26:34 -0300

    creating the team component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Banner from "./components/Banner"
 import Form from "./components/Form"
+import Team from "./components/Team"
 
 function App() {
   const [collaborators, setCollaborators] = useState([])
@@ -17,6 +18,9 @@ function App() {
           forTheNewCollaboratorAdded(collaborator)
         }
       />
+      <Team name="Programação" />
+      <Team name="Front-End" />
+      <Team name="Data Science" />
     </div>
   )
 }
