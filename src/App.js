a119cb027b8c0a1c5d8cba412d6b9d5d06c94e73
gabriel-vitebo <vitebo@outlook.com>
2023-03-24 15:22:15 -0300

    styling the team list

--- src/App.js
+++ src/App.js
@@ -4,6 +4,44 @@ import Form from "./components/Form"
 import Team from "./components/Team"
 
 function App() {
+  const teams = [
+    {
+      name: "Programação",
+      primaryColor: "#57c278",
+      secondaryColor: "#d9f7e9",
+    },
+    {
+      name: "Front-End",
+      primaryColor: "#82CFFA",
+      secondaryColor: "#E8F8FF",
+    },
+    {
+      name: "Data Science",
+      primaryColor: "#A6D157",
+      secondaryColor: "#F0F8E2",
+    },
+    {
+      name: "Devops",
+      primaryColor: "#E06B69",
+      secondaryColor: "#FDE7E8",
+    },
+    {
+      name: "UX e Design",
+      primaryColor: "#DB6EBF",
+      secondaryColor: "#FAE9F5",
+    },
+    {
+      name: "Mobile",
+      primaryColor: "#FFBA05",
+      secondaryColor: "#FFF5D9",
+    },
+    {
+      name: "Inovação e Gestão",
+      primaryColor: "#FF8A29",
+      secondaryColor: "#FFEEDF",
+    },
+  ]
+
   const [collaborators, setCollaborators] = useState([])
   const forTheNewCollaboratorAdded = (collaborator) => {
     console.log(collaborator)
@@ -18,9 +56,14 @@ function App() {
           forTheNewCollaboratorAdded(collaborator)
         }
       />
-      <Team name="Programação" />
-      <Team name="Front-End" />
-      <Team name="Data Science" />
+      {teams.map((team) => (
+        <Team
+          name={team.name}
+          key={team.name}
+          primaryColor={team.primaryColor}
+          secondaryColor={team.secondaryColor}
+        />
+      ))}
     </div>
   )
 }
