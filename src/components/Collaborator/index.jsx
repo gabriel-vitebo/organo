8a74f595e5617086cd99c7f75a6e0027d67cefe4
gabriel-vitebo <vitebo@outlook.com>
2023-03-27 18:03:19 -0300

    creating the contributor component

---
+++ src/components/Collaborator/index.jsx
@@ -0,0 +1,20 @@
+import "./Collaborator.css"
+
+const Collaborator = () => {
+  return (
+    <div className="collaborator">
+      <div className="header">
+        <img
+          src="https://github.com/gabriel-vitebo.png"
+          alt="Foto de perfil de Gabriel Vitebo"
+        />
+      </div>
+      <div className="footer">
+        <h4>Gabriel Vitebo</h4>
+        <h5>Aluno</h5>
+      </div>
+    </div>
+  )
+}
+
+export default Collaborator
--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -1,9 +1,11 @@
+import Collaborator from "../Collaborator"
 import "./Team.css"
 
 const Team = (props) => {
   return (
     <section className="team" style={{ backgroundColor: props.secondaryColor }}>
       <h3 style={{ borderColor: props.primaryColor }}>{props.name}</h3>
+      <Collaborator />
     </section>
   )
 }
