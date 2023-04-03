b1dc4878f4df9ceeb0c9545d57f87b35c0554a68
gabriel-vitebo <vitebo@outlook.com>
2023-04-03 14:11:03 -0300

    remove team list

--- src/App.js
+++ src/App.js
@@ -6,43 +6,7 @@ import Team from "./components/Team"
 import { v4 as uuidv4 } from "uuid"
 
 function App() {
-  const [teams, setTeams] = useState([
-    {
-      id: uuidv4(),
-      name: "Programação",
-      color: "#57c278",
-    },
-    {
-      id: uuidv4(),
-      name: "Front-End",
-      color: "#82CFFA",
-    },
-    {
-      id: uuidv4(),
-      name: "Data Science",
-      color: "#A6D157",
-    },
-    {
-      id: uuidv4(),
-      name: "Devops",
-      color: "#E06B69",
-    },
-    {
-      id: uuidv4(),
-      name: "UX e Design",
-      color: "#DB6EBF",
-    },
-    {
-      id: uuidv4(),
-      name: "Mobile",
-      color: "#FFBA05",
-    },
-    {
-      id: uuidv4(),
-      name: "Inovação e Gestão",
-      color: "#FF8A29",
-    },
-  ])
+  const [teams, setTeams] = useState([])
 
   const [collaborators, setCollaborators] = useState([])
   function handleRemoveCollaborator(collaboratorID) {
